f7aa6646405e656ff9bf6d8690e5d4b458eb2cea
Elliott Johnson <elliotthkj1991@gmail.com>
2024-05-03 18:40:06 -0700

    added styling and light/dark mode toggle

---
+++ src/components/ThemeSwitch.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { FiSun, FiMoon } from 'react-icons/fi';
+import { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+
+export default function ThemeSwitch() {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted)
+    return (
+      <Image
+        src='data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=='
+        width={16}
+        height={16}
+        sizes='36x36'
+        alt='Loading Light/Dark Toggle'
+        priority={false}
+        title='Loading Light/Dark Toggle'
+      />
+    );
+
+  return resolvedTheme === 'dark' ? (
+    <FiSun onClick={() => setTheme('light')} />
+  ) : (
+    <FiMoon onClick={() => setTheme('dark')} />
+  );
+}
--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -4,29 +4,59 @@ import { useChat } from 'ai/react';
 
 import { Button } from './ui/button';
 import { Input } from './ui/input';
+import { useRef, useEffect } from 'react';
+import ThemeSwitch from './ThemeSwitch';
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const chatParent = useRef<HTMLUListElement>(null);
+
+  // TODO: Customize the scrollbar styling
+  // TODO: Implement RAG w/ LangChain
+
+  useEffect(() => {
+    const domNode = chatParent.current;
+    if (domNode) {
+      domNode.scrollTop = domNode.scrollHeight;
+    }
+  });
 
   return (
     <main className='container flex flex-col w-full h-screen max-h-dvh bg-background py-4'>
-      {/* GREY+BLUE COLOR: bg-[#64748b] */}
-
-      <header className='p-4 border-b w-full max-w-3xl mx-auto'>
+      <header className='flex justify-between items-center p-4 border-b w-full max-w-3xl mx-auto'>
         <h1 className='text-3xl font-bold text-center'>AI Elli-bott</h1>
+        <ThemeSwitch />
       </header>
 
-      <section>
-        <ul>
+      <section className='container px-0 pb-10 flex flex-col flex-grow gap-4 mx-auto max-w-3xl'>
+        <ul
+          ref={chatParent}
+          className='h-1 p-4 flex-grow bg-muted/50 rounded-lg overflow-y-auto flex flex-col gap-4'
+        >
           {messages.map((m, index) => (
-            <li key={index}>
-              {m.role === 'user' ? 'Me: ' : 'Elli-bott: '}
-              {m.content}
-            </li>
+            <>
+              {m.role === 'user' ? (
+                <li key={index} className='flex flex-row-reverse'>
+                  <div className='rounded-xl p-4 bg-[#1987ff] shadow-md flex'>
+                    <p className='text-white'>{m.content}</p>
+                  </div>
+                </li>
+              ) : (
+                <li key={index} className='flex flex-row'>
+                  <div className='rounded-xl p-4 bg-background shadow-md flex w-3/4'>
+                    <p className='text-primary'>
+                      <span className='font-bold'>Elli-bott: </span>
+                      {m.content}
+                    </p>
+                  </div>
+                </li>
+              )}
+            </>
           ))}
         </ul>
       </section>
-      <section className='p-4 absolute inset-x-0 bottom-0'>
+
+      <section className='p-4'>
         <form
           onSubmit={handleSubmit}
           className='flex w-full max-w-3xl mx-auto items-center'
